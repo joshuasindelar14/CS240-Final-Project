b22a5347ef1f0ba11dfd986aefd96eca980226b1
Gabe Littrell <gabelittrell@gmail.com>
2025-05-07 12:57:38 -0700

    Create math.c

---
+++ math.c
@@ -0,0 +1,14 @@
+#include <math.h>
+int main(){
+    int x = 3;
+    int y = 5;
+    int z = x + y;
+
+    int a = math.pow(x, 2);
+    int b = math.sqrt(z);
+
+    printf("%f\n", a);
+    printf("%f\n", b);
+
+    return 0;
+}
